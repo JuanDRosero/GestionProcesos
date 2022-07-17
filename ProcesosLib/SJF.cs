f0714b8d01adc63694ead5d9c053ef61f5ee2b58
Juan David Rosero Torres <jdrosero59@gmail.com>
2022-07-17 14:02:43 -0500

    Correccción SJF

--- ProcesosLib/SJF.cs
+++ ProcesosLib/SJF.cs
@@ -10,17 +10,19 @@ namespace ProcesosLib
     {
         private List<ProcesoTiempo> procesos;
         private List<ProcesoTiempo> interrumpidos;
+        private List<ProcesoTiempo> procesosEliminar;
 
         public SJF()
         {
             procesos = new List<ProcesoTiempo>();
             interrumpidos = new List<ProcesoTiempo>();
+            procesosEliminar = new List<ProcesoTiempo>();
         }
 
-        public void agregarP(int id, int tiempoInicio, int tiempoFin)
+        public void agregarProceso(int id, int tiempoInicio, int tiempoFin)
         {
             var cond = procesos.Count == 0;
-            switch(id)
+            switch (id)
             {
                 case 1:
                     if (cond)
@@ -85,7 +87,7 @@ namespace ProcesosLib
             }
         }
 
-        public void interumpirprocesos(int id)   //Interrumpe el procesoss con el id suministrado (Si lo encuentra)
+        public void interumpirProceso(int id)   //Interrumpe el procesoss con el id suministrado (Si lo encuentra)
         {
             foreach (var item in procesos)
             {
@@ -107,7 +109,7 @@ namespace ProcesosLib
         }
         private void iniciarProceso(int id)     //Activa el proceso
         {
-            foreach (var item in interrumpidos)
+            foreach (var item in procesos)
             {
                 if (item.IDProceso == id)
                 {
@@ -117,9 +119,9 @@ namespace ProcesosLib
         }
         public void terminarProceso(int id)
         {
-            foreach(var item in procesos)
+            foreach (var item in procesos)
             {
-                if(item.IDProceso == id)
+                if (item.IDProceso == id)
                 {
                     item.Accion = item.Terminar;
                 }
@@ -136,7 +138,7 @@ namespace ProcesosLib
             {
                 item.Tick();
             }
-            procesos = procesos.OrderBy(x => x.tiempoTotal).ToList();
+            procesos = procesos.OrderBy(x => x.tiempoTotal).ToList();   //Ordena la lista de procesos en orden ascendente
             if (procesos.Count != 0)
             {
                 iniciarProceso(procesos.ElementAt(0).IDProceso);
@@ -154,35 +156,36 @@ namespace ProcesosLib
                         procesos.Remove(actual);
                         break;
                 }
-                if (procesos.Count > 1)
+                if (procesos.Count > 0 && procesos.ElementAt(0).tiempoRestante <= 0)
                 {
-                    for (int i = 0; i < procesos.Count; i++)
+                    terminarProceso(procesos.ElementAt(0).IDProceso);
+                }
+            }
+            if (interrumpidos.Count != 0)   //Revisa si se retomaron procesos interumpidos
+            {
+
+                foreach (var item in interrumpidos)
+                {
+                    if (item.Estado == Estado.Espera)
                     {
-                        procesos.ElementAt(i).tiempoFin++;
-                        procesos.ElementAt(i).tiempoRestante++;
+                        procesosEliminar.Add(item);
+                        procesos.Add(item);
+
                     }
                 }
-                if (interrumpidos.Count != 0)
-                {
-                    for (int i = 0; i < interrumpidos.Count; i++)
-                    {
-                        interrumpidos.ElementAt(i).tiempoFin++;
-                        interrumpidos.ElementAt(i).tiempoRestante++;
-                    }
-                }
+                procesosEliminar.ForEach(x => interrumpidos.Remove(x));
+                procesosEliminar.Clear();
             }
         }
 
         public string getProceso()
         {
-            if (procesos.Count != 0)
+            if (procesos.Count != 0 && procesos.ElementAt(0).Estado == Estado.Activo)
             {
                 return procesos.ElementAt(0).Nombre;
             }
             return "CPU Libre";
 
         }
-
-
     }
 }
